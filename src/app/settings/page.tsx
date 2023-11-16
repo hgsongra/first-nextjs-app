abda57cdb1111e99c1ca60f06e27c9b303e0a109
Hitesh Songra <hitesh-songra@pluralsight.com>
2023-11-16 12:50:36 +0530

    Setup the Next.js and added initial pages to render

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,12 @@
+
+import style from "./styles.module.css"
+
+export default function Settings() {
+  return (
+    <>
+   <h1 className={style.section}>
+    Welcome to Settings Page!
+   </h1>
+   </>
+  )
+}
